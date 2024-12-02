e573c182592438761ff8a5c67a6cdc53fe486e75
uthuyein <uthuyeinlinn@gmail.com>
2024-12-02 15:13:32 +0630

    update search

--- 02-jdbc-with-junit/src/main/java/com/jdc/mkt/services/UserService.java
+++ 02-jdbc-with-junit/src/main/java/com/jdc/mkt/services/UserService.java
@@ -2,6 +2,10 @@ package com.jdc.mkt.services;
 
 import static com.jdc.mkt.utils.DbConnection.getConnection;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.jdc.mkt.dto.User;
 
 public class UserService{
@@ -22,6 +26,51 @@ public class UserService{
 		String sql =  "delete from user_tbl where id = ?";
 		Object[] array = {id};
 		return execution(sql, array);
+	}
+	
+	public List<User> select(String name,String password,LocalDateTime from,LocalDateTime to) {
+		StringBuilder sb = new StringBuilder("select * from user_tbl where 1=1");
+		List<Object> temp = new ArrayList<>();
+		List<User> users = new ArrayList<User>();
+		
+		if(null != name) {
+			sb.append(" and username = ?");
+			temp.add(name);
+		}
+		if(null != password) {
+			sb.append(" and password = ?");
+			temp.add(password);
+		}
+		if(null != from) {
+			sb.append(" and create_date_time >= ? ");
+			temp.add(from);
+		}
+		if(null != to) {
+			sb.append(" and create_date_time <= ? ");
+			temp.add(to);
+		}
+		
+		try(var con = getConnection();
+				var stmt = con.prepareStatement(sb.toString()) ){
+			
+			for(int i = 0 ;i < temp.size() ; i ++) {			
+				stmt.setObject(i+1, temp.get(i));
+			}
+			var rs = stmt.executeQuery();
+			while(rs.next()) {
+				var user = new User(
+						rs.getInt("id"),
+						rs.getString("username"),
+						rs.getString("password"),
+						rs.getTimestamp("create_date_time").toLocalDateTime()					
+						);
+				users.add(user);				
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return users;
 	}
 	
 	public void resetUserTable() {
