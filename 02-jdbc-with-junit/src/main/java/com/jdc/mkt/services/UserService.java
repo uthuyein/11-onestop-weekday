c7b042e82d75b76d8ac9580c3682f037918a65d1
uthuyein <uthuyeinlinn@gmail.com>
2024-12-03 08:24:18 +0630

    update truncate table

--- 02-jdbc-with-junit/src/main/java/com/jdc/mkt/services/UserService.java
+++ 02-jdbc-with-junit/src/main/java/com/jdc/mkt/services/UserService.java
@@ -2,98 +2,95 @@ package com.jdc.mkt.services;
 
 import static com.jdc.mkt.utils.DbConnection.getConnection;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.jdc.mkt.dto.User;
 
-public class UserService{
-		
-	public int insert(User user) {
-		String sql =  "insert into user_tbl (id,username,password) values (?,?,?)";
-		Object[] array = {user.getId(),user.getUsername(),user.getPassword()};
-		return execution(sql, array);
+public class UserService {
+
+	public int insert(User user) throws SQLException {
+		String sql = "insert into user_tbl (username,password) values (?,?)";
+		Object[] array = { user.getUsername(), user.getPassword() };
+		return execution(sql, array).executeUpdate();
 	}
-	
-	public  int update(String updPass,int id) {
-		String sql =  "update user_tbl set password = ? where id = ?";
-		Object[] array = {updPass,id};
-		return execution(sql, array);
+
+	public int update(String updPass, int id) throws SQLException {
+		String sql = "update user_tbl set password = ? where id = ?";
+		Object[] array = { updPass, id };
+		return execution(sql, array).executeUpdate();
 	}
-	
-	public int delete(int id) {
-		String sql =  "delete from user_tbl where id = ?";
-		Object[] array = {id};
-		return execution(sql, array);
+
+	public int delete(int id) throws SQLException {
+		String sql = "delete from user_tbl where id = ?";
+		Object[] array = { id };
+		return execution(sql, array).executeUpdate();
 	}
-	
-	public List<User> select(String name,String password,LocalDateTime from,LocalDateTime to) {
+
+	public List<User> select(String name, String password, LocalDateTime from, LocalDateTime to) throws SQLException {
 		StringBuilder sb = new StringBuilder("select * from user_tbl where 1=1");
 		List<Object> temp = new ArrayList<>();
 		List<User> users = new ArrayList<User>();
-		
-		if(null != name) {
+
+		if (null != name) {
 			sb.append(" and username = ?");
 			temp.add(name);
 		}
-		if(null != password) {
+		if (null != password) {
 			sb.append(" and password = ?");
 			temp.add(password);
 		}
-		if(null != from) {
+		if (null != from) {
 			sb.append(" and create_date_time >= ? ");
 			temp.add(from);
 		}
-		if(null != to) {
+		if (null != to) {
 			sb.append(" and create_date_time <= ? ");
 			temp.add(to);
 		}
 		
-		try(var con = getConnection();
-				var stmt = con.prepareStatement(sb.toString()) ){
-			
-			for(int i = 0 ;i < temp.size() ; i ++) {			
-				stmt.setObject(i+1, temp.get(i));
+		var rs = execution(sb.toString(), temp.toArray()).executeQuery();
+	
+			while (rs.next()) {
+				var user = new User(rs.getInt("id"), rs.getString("username"), rs.getString("password"),
+						rs.getTimestamp("create_date_time").toLocalDateTime());
+				users.add(user);
 			}
-			var rs = stmt.executeQuery();
-			while(rs.next()) {
-				var user = new User(
-						rs.getInt("id"),
-						rs.getString("username"),
-						rs.getString("password"),
-						rs.getTimestamp("create_date_time").toLocalDateTime()					
-						);
-				users.add(user);				
-			}
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}		
+
+		
 		return users;
 	}
-	
-	public void resetUserTable() {
-		String sqlUser =  "truncate table user_tbl";
-		Object[] array = {};
-		execution(sqlUser, array);
-	}
-	
-	private  int execution(String sql,Object[] params) {
+
+	public void resetUserTable() throws SQLException {
 		try(var con = getConnection();
-				var stmt = con.prepareStatement(sql) ){
-			
-			for(int i = 0 ;i < params.length ; i ++) {			
-				stmt.setObject(i+1, params[i]);
-			}
-			var res = stmt.executeUpdate();
-			System.out.println("Res : "+res);
-			return res;
-			
+				var stmt = con.createStatement();) {		
+			stmt.execute("SET FOREIGN_KEY_CHECKS = 0");
+			stmt.execute("truncate table user_tbl");
+			stmt.execute("SET FOREIGN_KEY_CHECKS = 1");
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		return 0;
+	}
+
+	private PreparedStatement execution(String sql, Object[] params) {
+		try {
+			var con = getConnection();
+			var stmt = con.prepareStatement(sql);
+			
+			if(null != params)
+			for (int i = 0; i < params.length; i++) {
+				stmt.setObject(i + 1, params[i]);
+			}
+			return stmt;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 }
--- 02-jdbc-with-junit/src/test/java/com/jdc/mkt/UserTest.java
+++ 02-jdbc-with-junit/src/test/java/com/jdc/mkt/UserTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
@@ -21,20 +23,16 @@ public class UserTest {
 	private static UserService service;
 	
 	@BeforeAll
-	static void init() {
+	
+	static void init() throws SQLException {
 		service = new UserService();
-		
-	}
-	@Test
-	@Order(1)
-	void deleteTest() {
-		var res = service.delete(1);
-		assertEquals(1, res);
+		service.resetUserTable();
 	}
 	
+	
 	@Test
-	@Order(2)
-	void saveTest() {		
+	@Order(1)
+	void saveTest() throws SQLException {		
 		var res =  service.insert(new User(1,"User", "User", null));
 		assertNotNull(res);
 		assertTrue(res > 0);
@@ -43,9 +41,16 @@ public class UserTest {
 		assertNotEquals(2, res);
 	}
 	@Test
-	@Order(3)
-	void updateTest() {		
+	@Order(2)
+	void updateTest() throws SQLException {		
 		var res = service.update("123", 1);
+		assertEquals(1, res);
+	}
+	
+	@Test
+	@Order(3)
+	void deleteTest() throws SQLException {
+		var res = service.delete(1);
 		assertEquals(1, res);
 	}
 	
