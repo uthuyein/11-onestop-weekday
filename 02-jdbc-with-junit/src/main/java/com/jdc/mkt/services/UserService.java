3e91788017cebdeda7d52d26d5b39a5eb0447e38
uthuyein <uthuyeinlinn@gmail.com>
2024-12-03 08:40:18 +0630

    update generate keys

--- 02-jdbc-with-junit/src/main/java/com/jdc/mkt/services/UserService.java
+++ 02-jdbc-with-junit/src/main/java/com/jdc/mkt/services/UserService.java
@@ -2,8 +2,10 @@ package com.jdc.mkt.services;
 
 import static com.jdc.mkt.utils.DbConnection.getConnection;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,23 +13,37 @@ import java.util.List;
 import com.jdc.mkt.dto.User;
 
 public class UserService {
+	private Connection con;
+	private static PreparedStatement stmt;
 
 	public int insert(User user) throws SQLException {
 		String sql = "insert into user_tbl (username,password) values (?,?)";
 		Object[] array = { user.getUsername(), user.getPassword() };
-		return execution(sql, array).executeUpdate();
+		stmt = execution(sql, array);
+		stmt.executeUpdate();
+		var rs = stmt.getGeneratedKeys();
+		while(rs.next()) {
+			return rs.getInt(1);
+		}
+		closeConnection();
+		return 0;
 	}
 
 	public int update(String updPass, int id) throws SQLException {
 		String sql = "update user_tbl set password = ? where id = ?";
 		Object[] array = { updPass, id };
-		return execution(sql, array).executeUpdate();
+		int res = execution(sql, array).executeUpdate();
+		closeConnection();
+		return res;
+
 	}
 
 	public int delete(int id) throws SQLException {
 		String sql = "delete from user_tbl where id = ?";
 		Object[] array = { id };
-		return execution(sql, array).executeUpdate();
+		int res = execution(sql, array).executeUpdate();
+		closeConnection();
+		return res;
 	}
 
 	public List<User> select(String name, String password, LocalDateTime from, LocalDateTime to) throws SQLException {
@@ -51,46 +67,60 @@ public class UserService {
 			sb.append(" and create_date_time <= ? ");
 			temp.add(to);
 		}
-		
+
 		var rs = execution(sb.toString(), temp.toArray()).executeQuery();
-	
-			while (rs.next()) {
-				var user = new User(rs.getInt("id"), rs.getString("username"), rs.getString("password"),
-						rs.getTimestamp("create_date_time").toLocalDateTime());
-				users.add(user);
-			}
 
-		
-		return users;
-	}
-
-	public void resetUserTable() throws SQLException {
-		try(var con = getConnection();
-				var stmt = con.createStatement();) {		
-			stmt.execute("SET FOREIGN_KEY_CHECKS = 0");
-			stmt.execute("truncate table user_tbl");
-			stmt.execute("SET FOREIGN_KEY_CHECKS = 1");
-		}catch (Exception e) {
-			e.printStackTrace();
+		while (rs.next()) {
+			var user = new User(
+					rs.getInt("id"), 
+					rs.getString("username"), 
+					rs.getString("password"),
+					rs.getTimestamp("create_date_time").toLocalDateTime());
+			users.add(user);
 		}
-		
+		closeConnection();
+		return users;
 	}
 
 	private PreparedStatement execution(String sql, Object[] params) {
 		try {
-			var con = getConnection();
-			var stmt = con.prepareStatement(sql);
-			
-			if(null != params)
-			for (int i = 0; i < params.length; i++) {
-				stmt.setObject(i + 1, params[i]);
-			}
+			con = getConnection();
+			stmt = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+
+			if (null != params)
+				for (int i = 0; i < params.length; i++) {
+					stmt.setObject(i + 1, params[i]);
+				}
 			return stmt;
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 		return null;
+	}
+
+	public void resetUserTable() throws SQLException {
+		try {		
+			con = getConnection();
+			var stmt = con.createStatement();
+			stmt.execute("SET FOREIGN_KEY_CHECKS = 0");
+			stmt.execute("truncate table user_tbl");
+			stmt.execute("SET FOREIGN_KEY_CHECKS = 1");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		closeConnection();
+
+	}
+
+	private void closeConnection() throws SQLException {
+		
+		if(stmt != null && !stmt.isClosed()) {
+			stmt.close();
+		}
+		if (null != con && !con.isClosed()) {
+			con.close();
+		}
 	}
 }
