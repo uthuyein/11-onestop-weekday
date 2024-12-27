8652337d72cb6793c0b49629aaef565fdae64cc7
uthuyein <uthuyeinlinn@gmail.com>
2024-12-27 09:59:19 +0630

    update proj

--- 03-hibernate-jpa/ep05-jpa-query/src/main/java/com/jdc/mkt/entity/Department.java
+++ 03-hibernate-jpa/ep05-jpa-query/src/main/java/com/jdc/mkt/entity/Department.java
@@ -1,19 +1,21 @@
 package com.jdc.mkt.entity;
 
-import java.time.LocalDate;
+import java.util.List;
 
 import org.hibernate.annotations.ColumnDefault;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "employee_tbl")
-public class Employee {
+@Table(name = "department_tbl")
+public class Department {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,11 +24,9 @@ public class Employee {
 	@Column(nullable = false,length = 45)
 	private String name;
 	
-	@Column(nullable = false)
-	private LocalDate dob;
-	
 	@ColumnDefault("1")
 	private boolean active;
 	
-	private Department department;
+	@OneToMany(mappedBy = "department",cascade = CascadeType.PERSIST)
+	private List<Employee> employees;
 }
